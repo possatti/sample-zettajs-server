48721219601a24174f9d1432a43cee567432cc32
Lucas Possatti <lucas_possatti@hotmail.com>
2015-03-07 15:44:47 -0300

    Adicionada a célula fotovoltaica

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var zetta = require('zetta');
 var LED = require('zetta-led-mock-driver');
+var Photocell = require('zetta-photocell-mock-driver');
 
 zetta()
   .name('C-3PO')
   .use(LED)
+  .use(Photocell)
   .link('http://hello-zetta.herokuapp.com/')
   .listen(1337, function(){
     console.log('Zetta is running at http://127.0.0.1:1337');
