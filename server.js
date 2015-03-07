69be1b12f172e286437ebc5bcb2be6eee29192ef
Lucas Possatti <lucas_possatti@hotmail.com>
2015-03-07 15:31:33 -0300

    Conectando servidor local a uma nuvem

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var LED = require('zetta-led-mock-driver');
 zetta()
   .name('C-3PO')
   .use(LED)
+  .link('http://hello-zetta.herokuapp.com/')
   .listen(1337, function(){
-     console.log('Zetta is running at http://127.0.0.1:1337');
+    console.log('Zetta is running at http://127.0.0.1:1337');
 });
