f8d5b0c1d4e21b3eee21d246f1dead572721878a
Lucas Possatti <lucas_possatti@hotmail.com>
2015-03-08 02:43:32 -0300

    Adicionando o driver do banshee

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var zetta = require('zetta');
 var LED = require('zetta-led-mock-driver');
 var Photocell = require('zetta-photocell-mock-driver');
 var duskToDawnLight = require('./apps/dusk_to_dawn_light');
+var Banshee = require('zetta-banshee-driver');
 
 zetta()
   .name('C-3PO')
   .use(LED)
   .use(Photocell)
+  .use(Banshee)
   .use(duskToDawnLight)
   .link('http://hello-zetta.herokuapp.com/')
   .listen(1337, function(){
